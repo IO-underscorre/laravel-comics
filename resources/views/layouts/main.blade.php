4fbe029c63ddf66a3917a872c553caf708a69a36
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-11 08:08:06 +0200

    feat: added a favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <link rel="icon" type="image/ico" href="{!! asset('favicon.ico') !!}" />
+
     <title>@yield('page_title')</title>
 
     @vite('resources/js/app.js')
