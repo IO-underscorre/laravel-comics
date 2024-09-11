fec2f2f6b1f6cb9d706e2a5c90f33a9baf8d7039
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-11 05:27:11 +0200

    feat: top navigation bar completed

--- config/navmenues.php
+++ config/navmenues.php
@@ -3,8 +3,44 @@
 return [
     "main_nav_links" => [
         [
-            "route_name" => "home",
-            "menu_text" => "HOME",
+            "route_name" => "characters",
+            "menu_text" => "CHARACTERS",
+        ],
+        [
+            "route_name" => "comics",
+            "menu_text" => "COMICS",
+        ],
+        [
+            "route_name" => "movies",
+            "menu_text" => "MOVIES",
+        ],
+        [
+            "route_name" => "tv",
+            "menu_text" => "TV",
+        ],
+        [
+            "route_name" => "games",
+            "menu_text" => "GAMES",
+        ],
+        [
+            "route_name" => "collectibles",
+            "menu_text" => "COLLECTIBLES",
+        ],
+        [
+            "route_name" => "videos",
+            "menu_text" => "VIDEOS",
+        ],
+        [
+            "route_name" => "fans",
+            "menu_text" => "FANS",
+        ],
+        [
+            "route_name" => "news",
+            "menu_text" => "NEWS",
+        ],
+        [
+            "route_name" => "shop",
+            "menu_text" => "SHOP",
         ],
     ],
 ];
--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -3,21 +3,19 @@
 @endphp
 
 <nav class="topbar">
-    <ul>
-        <li class="logo">
-            <img src="{!! Vite::asset('resources/images/logo.png') !!}" alt="">
-        </li>
+    <div class="content-box">
+        <a href="{!! route('home') !!}" class="logo">
+            <img src="{!! Vite::asset('resources/images/dc-logo.png') !!}" alt="DC Comics">
+        </a>
 
-        <li>
-            <menu class="links">
-                @foreach ($main_nav_links as $link)
-                    <li class="{!! Route::currentRouteName() === $link['route_name'] ? 'active' : null !!}">
-                        <a href="{!! route($link['route_name']) !!}">
-                            {{ $link['menu_text'] }}
-                        </a>
-                    </li>
-                @endforeach
-            </menu>
-        </li>
-    </ul>
+        <menu>
+            @foreach ($main_nav_links as $link)
+                <li class="{!! Route::currentRouteName() === $link['route_name'] ? 'active' : null !!}">
+                    <a href="{!! route($link['route_name']) !!}">
+                        {{ $link['menu_text'] }}
+                    </a>
+                </li>
+            @endforeach
+        </menu>
+    </div>
 </nav>
